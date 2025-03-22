a0feed119d7c03fd8e512b51552eb5d507f314ad
cnedienne <cbouzid49@gmail.com>
2025-03-22 20:48:20 +0100

    début mission 2 tache 1

---
+++ MediaTekDocuments/controller/FrmMediatekController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using MediaTekDocuments.model;
+using MediaTekDocuments.dal;
+
+namespace MediaTekDocuments.controller
+{
+    /// <summary>
+    /// Contrôleur lié à FrmMediatek
+    /// </summary>
+    class FrmMediatekController
+    {
+        /// <summary>
+        /// Objet d'accès aux données
+        /// </summary>
+        private readonly Access access;
+
+        /// <summary>
+        /// Récupération de l'instance unique d'accès aux données
+        /// </summary>
+        public FrmMediatekController()
+        {
+            access = Access.GetInstance();
+        }
+
+        /// <summary>
+        /// getter sur la liste des genres
+        /// </summary>
+        /// <returns>Liste d'objets Genre</returns>
+        public List<Categorie> GetAllGenres()
+        {
+            return access.GetAllGenres();
+        }
+
+        /// <summary>
+        /// getter sur la liste des livres
+        /// </summary>
+        /// <returns>Liste d'objets Livre</returns>
+        public List<Livre> GetAllLivres()
+        {
+            return access.GetAllLivres();
+        }
+
+        /// <summary>
+        /// getter sur la liste des Dvd
+        /// </summary>
+        /// <returns>Liste d'objets dvd</returns>
+        public List<Dvd> GetAllDvd()
+        {
+            return access.GetAllDvd();
+        }
+
+        /// <summary>
+        /// getter sur la liste des Commandes
+        /// </summary>
+        /// <returns>Liste d'objets Commandes</returns>
+        public List<CommandeDocument> GetAllCommandes(string id)
+        {
+            return access.GetAllCommandes(id);
+        }
+
+        /// <summary>
+        /// getter sur la liste des revues
+        /// </summary>
+        /// <returns>Liste d'objets Revue</returns>
+        public List<Revue> GetAllRevues()
+        {
+            return access.GetAllRevues();
+        }
+
+        /// <summary>
+        /// getter sur les rayons
+        /// </summary>
+        /// <returns>Liste d'objets Rayon</returns>
+        public List<Categorie> GetAllRayons()
+        {
+            return access.GetAllRayons();
+        }
+
+        /// <summary>
+        /// getter sur les publics
+        /// </summary>
+        /// <returns>Liste d'objets Public</returns>
+        public List<Categorie> GetAllPublics()
+        {
+            return access.GetAllPublics();
+        }
+
+
+        /// <summary>
+        /// récupère les exemplaires d'une revue
+        /// </summary>
+        /// <param name="idDocuement">id de la revue concernée</param>
+        /// <returns>Liste d'objets Exemplaire</returns>
+        public List<Exemplaire> GetExemplairesRevue(string idDocuement)
+        {
+            return access.GetExemplairesRevue(idDocuement);
+        }
+
+        /// <summary>
+        /// Crée un exemplaire d'une revue dans la bdd
+        /// </summary>
+        /// <param name="exemplaire">L'objet Exemplaire concerné</param>
+        /// <returns>True si la création a pu se faire</returns>
+        public bool CreerExemplaire(Exemplaire exemplaire)
+        {
+            return access.CreerExemplaire(exemplaire);
+        }
+
+        /// <summary>
+        /// Créer une commande d'un livre dans la BDD
+        /// </summary>
+        /// <param name="commande">L'objet Commande concerné</param>
+        /// <returns>True si la création a pu se faire</returns>
+        public bool CreerCommande(Commande commande)
+        {
+            return access.CreerCommande(commande);
+        }
+
+        /// <summary>
+        /// Crée une commande de livre dans la bdd
+        /// </summary>
+        /// <param name="commandedocument">L'objet commande concerné</param>
+        /// <returns>True si la création a pu se faire</returns>
+        public bool CreerCommandeDocument(string id, int nbExemplaire, string idLivreDvd, string idSuivi)
+        {
+            return access.CreerCommandeDocument(id, nbExemplaire, idLivreDvd, idSuivi);
+        }
+
+        /// <summary>
+        /// Supprimer une commande de livre dans la bdd
+        /// </summary>
+        /// <param name="commandedocument">L'objet commande concerné</param>
+        /// <returns>True si la suppression a pu se faire</returns>
+        public bool SupprimerCommande(string id)
+        {
+            return access.SupprimerCommande(id);
+        }
+
+        /// <summary>
+        /// Modifie l'étape de suivi d'une commande
+        /// </summary>
+        /// <param name="id">Id de la commande de document à modifier</param>
+        /// <param name="idSuivi">Id de l'étape de suivi</param>
+        /// <returns>True si la création a pu se faire</returns>
+        public bool ModifierSuiviCommandeDocument(string id, string idSuivi)
+        {
+            return access.ModifierSuiviCommandeDocument(id, idSuivi);
+        }
+    }
+}
---
+++ MediaTekDocuments/model/CommandeDocument.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTekDocuments.model
+{
+    /// <summary>
+    /// Classe métier CommandeDocument qui hérite de Commande
+    /// </summary>
+    public class CommandeDocument : Commande
+    {
+
+        public int nbExemplaire { get; set; }
+        public string idSuivi { get; set; }
+        public string etapeSuivi { get; set; }
+        public string idLivreDvd { get; set; }
+
+        public CommandeDocument(string id, DateTime dateCommande, double montant, int nbExemplaire,
+            string idSuivi, string idDvdLivre)
+            : base(id, dateCommande, montant)
+        {
+            this.nbExemplaire = nbExemplaire;
+            this.idSuivi = idSuivi;
+            this.idLivreDvd = idDvdLivre;
+        }
+    }
+}
